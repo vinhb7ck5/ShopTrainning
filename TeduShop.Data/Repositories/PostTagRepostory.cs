8d3715cce4d1df5ecacbd443bb30e25a52d9cdd6
vhg19 <vhg19@DESKTOP-BNVD6T3>
2019-07-25 01:15:15 +0700

    add more repo

---
+++ TeduShop.Data/Repositories/PostTagRepostory.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IPostTagRepository
+    {
+    }
+
+    public class PostTagRepostory : RepositoryBase<PostTag>, IPostTagRepository
+    {
+        public PostTagRepostory(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ TeduShop.Data/Repositories/VisitorStaticRepository.cs
@@ -0,0 +1,16 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IVisitorStaticRepository
+    {
+    }
+
+    public class VisitorStaticRepository : RepositoryBase<VisitorStatic>, IVisitorStaticRepository
+    {
+        public VisitorStaticRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
